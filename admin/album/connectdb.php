f96879a1841a561536a7ddf8b751ce957df7f405
Kanyapa2538 <65010912538@msu.ac.th>
2024-10-20 12:55:16 +0700

    edit

--- admin/album/connectdb.php
+++ admin/album/connectdb.php
@@ -1,11 +1,11 @@
 <?php
-$host = "localhost";
-$usr = "root";
-$pwd = "ip123456789"; // เปลี่ยนเป็นรหัสผ่านที่ถูกต้อง
-$db = "cosmeticnew";
+    $host = "localhost" ;
+	$usr = "root";
+	$pwd = "ip123456789";
+	$db = "cosmeticnew";
 
-// เชื่อมต่อฐานข้อมูล
-$conn = mysqli_connect($host, $usr, $pwd) or die("เชื่อมต่อฐานข้อมูลไม่ได้: " . mysqli_connect_error());
-mysqli_select_db($conn, $db) or die("เลือกฐานข้อมูลไม่ได้: " . mysqli_error($conn));
-mysqli_query($conn, "SET NAMES utf8");
-?>
+	$conn = mysqli_connect($host,$usr,$pwd) or die("เชื่อมต่อฐานข้อมูลไม่ได้");
+	mysqli_select_db($conn,$db) or die("เลือกฐานข้อมูลไม่ได้");
+	mysqli_query($conn,"SET NAMES utf8");
+	?>
+	
